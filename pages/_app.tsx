b9054051f0b8a1ff7e0d6ed173c53ee5ca82ec91
Abiel Zulio M <abielzm@gmail.com>
2023-01-13 21:10:24 +0700

    feat: add react-hot-toaster

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import Main from "components/Main"
+import { Toaster } from "react-hot-toast"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Main>
+      <Toaster />
       <Component {...pageProps} />
     </Main>
   )
