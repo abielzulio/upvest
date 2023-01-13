aff3316997e2095f661763b51e99702a3df67880
Abiel Zulio M <abielzm@gmail.com>
2023-01-14 00:04:08 +0700

    feat: UserContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import Main from "components/Main"
 import { Toaster } from "react-hot-toast"
+import { UserProvider } from "context/user"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Main>
-      <Toaster />
-      <Component {...pageProps} />
-    </Main>
+    <UserProvider>
+      <Main>
+        <Toaster />
+        <Component {...pageProps} />
+      </Main>
+    </UserProvider>
   )
 }
