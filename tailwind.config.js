a5629864aead2efe5ff12833a17139ffce9e8040
Abiel Zulio M <abielzm@gmail.com>
2023-01-14 16:08:41 +0700

    style: add bg image for stock

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        stock:
+          "linear-gradient(273.58deg, rgba(34, 197, 94, 0.1) 0%, rgba(34, 197, 94, 0.05) 100%)",
+      },
       colors: {
         white: "#f2efdc",
       },
