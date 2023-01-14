d5ebfb6d5ad01f349ea11d557eef3dbb74846901
Abiel Zulio M <abielzm@gmail.com>
2023-01-14 13:53:20 +0700

    style: Main shadow

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,6 +1,6 @@
 const Main = ({ children }: { children: React.ReactNode }) => (
   <main className="w-screen bg-white sm:px-[24px]">
-    <section className="sm:max-w-[500px] relative h-screen-safe m-auto sm:border-[1px] overflow-hidden sm:border-[#ebebeb] sm:shadow-lg bg-white sm:rounded-xl flex flex-col">
+    <section className="sm:max-w-[500px] relative h-screen-safe m-auto sm:border-[3px] overflow-hidden sm:border-black/10 sm:shadow-2xl bg-white sm:rounded-xl flex flex-col">
       {children}
     </section>
   </main>
